65a421a8ed2efacd12e75e2bda3135b50eafcd07
APG <asu-active-perception-group@googlegroups.com>
2017-06-16 17:48:47 -0700

    Add clean hand tracking code

--- obj_hand_detect_track.py
+++ obj_hand_detect_track.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+import time
 
 
 # Function to find angle between two vectors
@@ -17,8 +18,8 @@ def FindDistance(A,B):
 # open camera object
 
 cap = cv2.VideoCapture('output3.avi');
-fourcc = cv2.cv.CV_FOURCC('I','4','2','0')
-video = cv2.VideoWriter('out.avi',fourcc,25,(1152,848));
+fourcc = cv2.cv.CV_FOURCC('I', '4', '2', '0')
+video = cv2.VideoWriter('hand_object_tracking.avi', fourcc, 25.0, (1152, 848));
 
 # Decrease frame size
 
@@ -26,22 +27,24 @@ cap.set(cv2.cv.CV_CAP_PROP_FRAME_WIDTH,1000)
 cap.set(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT,600)
 i =0
 no = 0
+# total = 0
+# red = 0
+
 while 1:
 
     # Take each frame
 
     ret, frame = cap.read()
-    cropped = frame[0:600,0:600]
-    cv2.imshow("Cropped",cropped)
+    cropped = frame[0:600, 0:590]
+    #cv2.imshow("Cropped", cropped)
 
-    ############################################################################################################################
-    ######################################OBJECT DETECTION######################################################################
+    ####################################################################################################################
+    ######################################OBJECT DETECTION##############################################################
     # Convert BGR to HSV
 
     hsv = cv2.cvtColor(cropped, cv2.COLOR_BGR2HSV)
 
     # Red Mask
-
     # define range of red color in HSV
     lower_red = np.array([0, 100, 100])
     upper_red = np.array([10, 255, 255])
@@ -78,10 +81,11 @@ while 1:
     greenContours, greenHierarchy = cv2.findContours(greenMask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     blueContours, blueHierarchy = cv2.findContours(blueMask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     yellowContours, yellowHierarchy = cv2.findContours(yellowMask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    total = 0
-    greenTotal = 0
     blueTotal = 0
+    greenTotal = 0
+    redTotal = 0
     yellowTotal = 0
+    font = cv2.FONT_HERSHEY_SIMPLEX
     #cv2.drawContours(frame,redContours, -1, (0, 100, 100), 3)
 
 
@@ -92,11 +96,12 @@ while 1:
         redApprox = cv2.approxPolyDP(c, 0.08*redPeri, True)
         redArea = cv2.contourArea(c)
         if redArea > 350:
-            cv2.drawContours(frame, [redApprox],-1,(255, 255,255),4)
+            #cv2.drawContours(frame, [redApprox],-1,(255, 255,255),4)
             x, y, w, h = cv2.boundingRect(c)
             cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 255, 255), 2)
-            total = total+1
-
+            redTotal = redTotal + 1
+        # if(redTotal > red):
+        #     red = redTotal
 
     # loop over the blue contours
     for c in blueContours:
@@ -105,8 +110,10 @@ while 1:
         blueApprox = cv2.approxPolyDP(c, 0.08* bluePeri, True)
         blueArea=cv2.contourArea(c)
         if blueArea > 350:
-            cv2.drawContours(frame, [blueApprox], -1, (255, 255, 0), 4)
+            #cv2.drawContours(frame, [blueApprox], -1, (255, 255, 0), 4)
             x, y, w, h = cv2.boundingRect(c)
+            w = 32
+            h = 32
             cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 255, 0), 2)
             blueTotal = blueTotal + 1
 
@@ -115,13 +122,13 @@ while 1:
         # approximate the contour
         greenPeri = cv2.arcLength(c, True)
         greenApprox = cv2.approxPolyDP(c, 0.08 * greenPeri, True)
-
         greenArea = cv2.contourArea(c)
+        currGreenTotal = greenTotal
         if greenArea > 350:
-            cv2.drawContours(frame, [greenApprox], -1, (0, 255, 0), 4)
+            #cv2.drawContours(frame, [greenApprox], -1, (0, 255, 0), 4)
             x, y, w, h = cv2.boundingRect(c)
             cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-            greenTotal = greenTotal + 1
+            greenTotal = 1 + greenTotal
 
     # loop over the yellow contours
     for c in yellowContours:
@@ -130,20 +137,30 @@ while 1:
         yellowApprox = cv2.approxPolyDP(c, 0.08 * yellowPeri, True)
         yellowArea = cv2.contourArea(c)
         if yellowArea > 600 and yellowArea<2000:
-            cv2.drawContours(frame, [yellowApprox], -1, (255, 0, 255), 4)
+            #cv2.drawContours(frame, [yellowApprox], -1, (255, 0, 255), 4)
             x, y, w, h = cv2.boundingRect(c)
+            w = 32
+            h = 32
             cv2.rectangle(frame, (x, y), (x + w, y + h), (255,0, 255), 2)
             yellowTotal = yellowTotal + 1
 
-    print('Total Number of blocks')
-    print(yellowTotal+greenTotal+total+blueTotal)
+    total = blueTotal + greenTotal + yellowTotal + redTotal
+    # if(currTotal > total):
+    #     total = currTotal
+    print('Total Number of blocks: ' + str(total))
+    cv2.putText(frame, "num total " + str(total), (50, 40), font, 0.8, (255, 255, 255), 2)
+    cv2.putText(frame, "num red " + str(redTotal), (50, 80), font, 0.8, (255, 255, 255), 2)
+    cv2.putText(frame, "num blue " + str(blueTotal), (50, 120), font, 0.8, (255, 255, 255), 2)
+    cv2.putText(frame, "num yellow " + str(yellowTotal), (50, 160), font, 0.8, (255, 255, 255), 2)
+    cv2.putText(frame, "num green " + str(greenTotal), (50, 200), font, 0.8, (255, 255, 255), 2)
+
     #cv2.imshow('frame', frame)
     #cv2.waitKey(20)
 
 
 
-############################################################################################################################
-######################################HAND TRACKING######################################################################
+########################################################################################################################
+######################################HAND TRACKING#####################################################################
 
 
     #Blur the image
@@ -177,8 +194,8 @@ while 1:
     contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
 
     #Draw Contours
-    # cv2.drawContours(frame, cnt, -1, (122,122,0), 3)
-    cv2.imshow('Dilation',median)
+    #cv2.drawContours(frame, cnt, -1, (122,122,0), 3)
+    #cv2.imshow('Dilation',median)
 
 	#Find Max contour area (Assume that hand is in the frame)
     max_area=100
@@ -209,7 +226,7 @@ while 1:
         far = tuple(cnts[f][0])
         FarDefect.append(far)
         cv2.line(frame,start,end,[0,255,0],1)
-        cv2.circle(frame,far,10,[100,255,255],3)
+        #cv2.circle(frame,far,10,[100,255,255],3)
 
 	#Find moments of the largest contour
     moments = cv2.moments(cnts)
@@ -222,7 +239,6 @@ while 1:
 
     #Draw center mass
     cv2.circle(frame,centerMass,7,[100,0,255],2)
-    font = cv2.FONT_HERSHEY_SIMPLEX
     cv2.putText(frame,'Center',tuple(centerMass),font,2,(255,255,255),2)
 
     #Distance from each finger defect(finger webbing) to the center mass
@@ -263,7 +279,7 @@ while 1:
             result = result +1
 
     #Print number of pointed fingers
-    cv2.putText(frame,str(result),(100,100),font,2,(255,255,255),2)
+    #cv2.putText(frame,str(result),(100,100),font,2,(255,255,255),2)
 
     #Print bounding rectangle
     x,y,w,h = cv2.boundingRect(cnts)
@@ -282,11 +298,9 @@ while 1:
     #close the output video by pressing 'ESC'
     k = cv2.waitKey(20) & 0xFF
 
-
-
     if k == 27:
         break
 
-
 cap.release()
+video.release()
 cv2.destroyAllWindows()
